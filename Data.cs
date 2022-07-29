b4613011a58cd90a9ecd9ab42b1f83489101a273
Saqlainabbas760 <50928063+Saqlainabbas760@users.noreply.github.com>
2022-07-29 15:51:55 +0500

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Data
+{
+
+}
+//{
+//    public static MediaBundle imagesBundle { get; set; }
+//    public static MediaBundle urls { get; set; }
+//}
